319edbd147f7c191f0a0a3facee2a5c8c1992713
Piotr Wójcik <chocimier@tlen.pl>
2020-04-04 00:41:02 +0200

    Add tool to find novel collections

---
+++ tools/novel_collections.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+# pkgs.void - web catalog of Void Linux packages.
+# Copyright (C) 2020 Piotr Wójcik <chocimier@tlen.pl>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import sys
+
+sys.path.append('.')
+
+import datasource # noqa, pylint: disable=wrong-import-position
+from custom_types import Interest # noqa, pylint: disable=wrong-import-position
+
+SOURCE = datasource.factory()
+COLLECTIONS = SOURCE.metapackages()
+for i in COLLECTIONS:
+    if Interest(i['classification']) == Interest.NOVEL:
+        print(i['pkgname'])
