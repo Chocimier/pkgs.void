d4db17059812ab07e8abc7a8dc0b48cb4abeed0f
Piotr Wójcik <chocimier@tlen.pl>
2022-03-21 23:51:35 +0100

    [docker] Add serve mode of fcgi binding into socket

--- serve.py
+++ serve.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from sys import argv, exit as sys_exit
+from sys import argv, exit as sys_exit, stderr
 
 from app import app
 from settings import config
@@ -26,6 +26,16 @@ if __name__ == '__main__':
     elif argv[1] == 'flup_socket':
         from flup.server.fcgi import WSGIServer
         WSGIServer(app).run()
+    elif argv[1] == 'flup_bind':
+        from flup.server.fcgi import WSGIServer
+        if len(argv) == 4:
+            address = (argv[2], int(argv[3]))
+        elif len(argv) == 3:
+            address = argv[2]
+        else:
+            print('pass unix socket path or interface and port', file=stderr)
+            sys_exit(1)
+        WSGIServer(app, bindAddress=address).run()
     elif argv[1] == 'cgi':
         from wsgiref.handlers import CGIHandler
         CGIHandler().run(app)
